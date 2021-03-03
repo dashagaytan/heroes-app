99e545a6eccfc00ddd4aa0f419319df8cefbca21
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2021-03-02 23:07:57 -0600

    styled heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,13 +7,19 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   
   heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero =hero;
   }
 
 }
